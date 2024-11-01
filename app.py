dbbae8497dd132e47aebf701deae48fb7909ef04
ramosmanel <emanuelramos20004@gmail.com>
2024-11-01 15:15:46 -0300

    feat: token config

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
+from crypt import methods
+
 from flask import Flask, request, jsonify, make_response, render_template, session
 import jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from functools import wraps
 import os
 
+from pycparser.ply.yacc import token
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+def token_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired!'}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({'message': 'Invalid token!'}), 401
+
+        return func(*args, **kwargs)
+    return decorated
 
 @app.route('/')
 def home():
@@ -14,6 +36,19 @@ def home():
     else:
         return 'Logged in currently!'
 
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['username'] and request.form['password'] == 'MyPassword123':
+        session['logged_in'] = True
+
+        token = jwt.encode({
+            'user': request.form['username'],
+            'expiration': (datetime.now(timezone.utc) + timedelta(seconds=120)).isoformat()
+        }, app.config['SECRET_KEY'], algorithm="HS256")
+        return jsonify({'token': token})
+    else:
+        return make_response('Unable to verify', 403,
+                             {'WWW-Authenticate': 'Basic realm: "Authentication failed!"'})
 
 if __name__  == "__main__":
     app.run(debug=True)
